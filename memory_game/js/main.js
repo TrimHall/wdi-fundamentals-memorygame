21ea902f1ccb43f9f1cc0885bf0012d93389a9f6
Tristan Hall <tristanhall@tristans-mbp.mynet>
2018-05-27 21:58:30 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+//console.log("User flipped" + cardThree);
+//console.log("User flipped" + cardOne);
 
-console.log("User flipped" + cardThree);
-console.log("User flipped" + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("user flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) { 
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!"); 
+	} else {
+	alert("Sorry, try again");
+}
+}
