1ae349d2cfbf63f17636b8fb36f847381b2bf900
Tristan Hall <tristanhall@tristans-mbp.cogs.local>
2018-06-04 10:12:23 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,3 @@
-//var cards = ["queen", "queen", "king", "king"];
 var cards = [
   {
   	rank: "queen",
@@ -31,13 +30,26 @@ var checkForMatch = function() {
 } 
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
-	checkForMatch();
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	} 
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+}
+};
+
+createBoard();
